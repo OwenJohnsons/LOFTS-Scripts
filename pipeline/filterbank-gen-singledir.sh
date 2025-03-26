794def7837c45e001b374236767a2e55e62d3a90
owenjohnsons <ojohnson@tcd.ie>
2025-03-26 18:24:31 +0100

    Fixed file name parsing issue.

--- pipeline/filterbank-gen-singledir.sh
+++ pipeline/filterbank-gen-singledir.sh
@@ -22,14 +22,16 @@ log_name=$(echo $path | awk -F'/' '{print $NF}')
 log_file="/datax2/projects/LOFTS/data/LOFTS/logs/filgen/${log_name}_${time}.out"
 error_file="/datax2/projects/LOFTS/data/LOFTS/logs/filgen/${log_name}_${time}.err"
 
-folders=$(find $path -type d \( -name "*LOFTS*" -o -name "*B*" \))
-date=$(echo $path | awk -F'/' '{print $(NF)}' | sed 's/\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\).*/\1-\2-\3/')
+folders=$(find $path -type d \( -name "*scan_LOFTS*" -o -name "*scan_B*" \))
+date=$(echo $path | awk -F'/' '{print $(NF)}' | sed 's/^.*sid\([0-9]\{8\}\)T.*/\1/' | sed 's/\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)/\1-\2-\3/')
+
 echo "Number of folders found : $(echo $folders | wc -w)"
 echo "Date of observations: $date"
 
 for folder in $folders; do 
+    echo "Processing folder: $folder"
     # last part of the path ignore the first 8 characters
-    target=$(echo $folder | awk -F'/' '{print $NF}' | cut -c 15-)
+    target=$(echo $folder | awk -F'/' '{print $NF}' | sed 's/^.\{5\}//')
     echo "Processing Target : $target"
 
     output_dir="/datax2/projects/LOFTS/$date/$target"
@@ -51,7 +53,8 @@ for folder in $folders; do
         zst_scan_path=$(echo "${voltages[0]}" | sed "s/udp_${port_prefix}0/udp_${port_prefix}[[port]]/")
         echo "Scan path: $zst_scan_path"
 
-        metadata=${folder}/metadata.h
+
+        metadata=$(find $folder -name "*.h")
 
         raw_files=$(find $output_dir -name "*.raw")
         if [ ! -z "$raw_files" ]; then 
@@ -72,6 +75,7 @@ for folder in $folders; do
     if [ -f "$output_dir/${target}.rawspec.0000.fil" ] && [ -f "$output_dir/${target}.rawspec.0001.fil" ] && [ -f "$output_dir/${target}.rawspec.0002.fil" ]; then
         echo "Filterbank files 0000.fil, 0001.fil, and 0002.fil exist in $output_dir. Skipping rawspec..."
     else
+        echo "Running rawspec..."
         # Run rawspec if the required .fil files do not exist
         rawspec -f 65536,8,64 -t 54,16,3072 -p 1,1,4 $output_dir/${target}
     fi
@@ -81,5 +85,5 @@ for folder in $folders; do
         echo "Cleaning raw files..."
         rm -f $output_dir/${target}*.raw
     fi
-
+    echo "Science ready data products generated for $target" | sudo wall 
 done
